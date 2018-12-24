66e0dd8dcf54097d6038ae220b11dce6dff9a064
sam wronski <srwronsk@mtu.edu>
2018-12-24 11:28:42 -0600

    Add embedded video player.

--- frontend/components/VideoInformationPanel.js
+++ frontend/components/VideoInformationPanel.js
@@ -2,6 +2,7 @@ import { withStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
 import Markdown from 'react-markdown'
 import CodeBlock from '../renderers/CodeBlock'
+import YouTube from 'react-youtube'
 
 const mdx = '# Hello World\n\n```csharp\nConsole.WriteLine("Hello World");\n```\n\n*more testing* what about word wrapping? How does that work? Maybe it will actually justify like I want... but maybe not.'
 
@@ -11,7 +12,7 @@ export default class VideoInformationPanel extends React.Component {
     return (
       <Grid container>
         <Grid item md={8} xs={12}>
-        test
+          <YouTube videoId="_08No6ET-qk"/>
         </Grid>
         {/* Justify the markdown description. */}
         <Grid style={{ textAlign: 'justify' }} item md={4} xs={12}>
