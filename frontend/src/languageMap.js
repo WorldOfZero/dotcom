8da75eb8c69c627e85de25635b5e33281530ba75
sam wronski <srwronsk@mtu.edu>
2018-12-26 16:55:21 -0600

    Add language bar to make it easier to see what language code is in. Code is color coded with a link to a learning resource provided.
    Current languages:
    - C#
    - Java
    - Go
    - Shaders

---
+++ frontend/src/languageMap.js
@@ -0,0 +1,22 @@
+export const languageMap = {
+  csharp: {
+    name: "C#",
+    color: "#551A8B",
+    link: "https://try.dot.net"
+  },
+  java: {
+    name: "Java",
+    color: "#FF0000",
+    link: "https://www.compilejava.net"
+  },
+  shader: {
+    name: "Graphics Shader",
+    color: "#a31545",
+    link: "https://shadertoy.com"
+  },
+  go: {
+    name: "Go",
+    color: "#666666",
+    link: "https://tour.golang.org/"
+  }
+}
