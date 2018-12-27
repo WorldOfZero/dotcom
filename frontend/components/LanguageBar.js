ff62eed3c83ccc81fa5a93840a3f59dc5c2edba4
sam wronski <srwronsk@mtu.edu>
2018-12-26 20:10:30 -0600

    Open language help in new tab/window

--- frontend/components/LanguageBar.js
+++ frontend/components/LanguageBar.js
@@ -12,7 +12,7 @@ export default class LanguageBar extends React.Component {
             <strong>{ name }</strong>
           </Grid>
           <Grid style={{ textAlign: 'right' }} item sm={6} xs={12}>
-            <a href={link} style={{ color: '#FFFFFF' }}>Explore This Language</a>
+            <a href={link} target="_blank" style={{ color: '#FFFFFF' }}>Explore This Language</a>
           </Grid>
         </Grid>
       </div>
