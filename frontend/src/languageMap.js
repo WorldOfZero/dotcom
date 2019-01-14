b57f6270b05c9fd731ccf9d4dd50ace8e4d2265d
Sam Wronski <sam@worldofzero.com>
2019-01-13 20:37:38 -0800

    Add and tweak supported languages

--- frontend/src/languageMap.js
+++ frontend/src/languageMap.js
@@ -4,19 +4,49 @@ export const languageMap = {
     color: "#551A8B",
     link: "https://try.dot.net"
   },
+  docker: {
+    name: "Docker",
+    color: "#007BFF",
+    link: "https://www.docker.com"
+  },
+  fsharp: {
+    name: "F#",
+    color: "#551A8B",
+    link: "https://try.dot.net"
+  },
   java: {
     name: "Java",
     color: "#FF0000",
     link: "https://www.compilejava.net"
+  },
+  js: {
+    name: "Javascript",
+    color: "#F9A825",
+    link: "https://glitch.com/edit/#!/remix/hello-express"
+  },
+  javascript: {
+    name: "Javascript",
+    color: "#F9A825",
+    link: "https://glitch.com/edit/#!/remix/hello-express"
+  },
+  python: {
+    name: "Python",
+    color: "#6B8E23",
+    link: "http://codepad.org"
+  },
+  go: {
+    name: "Go",
+    color: "#666666",
+    link: "https://tour.golang.org"
   },
   shader: {
     name: "Graphics Shader",
     color: "#a31545",
     link: "https://shadertoy.com"
   },
-  go: {
-    name: "Go",
-    color: "#666666",
-    link: "https://tour.golang.org/"
+  regex: {
+    name: "Regular Expression",
+    color: "#8B0000",
+    link: "https://regex101.com"
   }
 }
