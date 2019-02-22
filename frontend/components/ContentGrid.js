c53c9e178d182fea01a501e389f32a1af76c700c
Sam Wronski <sam@worldofzero.com>
2019-02-21 21:12:35 -0800

    Display Videos in Grid

--- frontend/components/ContentGrid.js
+++ frontend/components/ContentGrid.js
@@ -1,6 +1,7 @@
 import { GridList, GridListTile } from '@material-ui/core';
 import ContentGridItem from './ContentGridItem';
 import Measure from 'react-measure'
+import Link from 'next/link'
 
 export default class ContentGrid extends React.Component {
   static defaultProps = {
@@ -12,8 +13,22 @@ export default class ContentGrid extends React.Component {
     super(props);
     this.state = {
       columns: 6,
-      rowHeight: 160
+      rowHeight: 160,
+      videos: []
     };
+  }
+
+  componentDidMount() {
+    fetch("https://localhost:5001/api/video").then(results => {
+      return results.json();
+    }).then(results => {
+      console.log(results);
+      const videoViews = [];
+      results.videos.forEach(video => {
+        videoViews.push({id: video.id, title: video.title, img: video.thumbnail});
+      });
+      this.setState({videos: videoViews});
+    })
   }
 
   matchHeightToRatio(width, aspectRatio) {
@@ -23,47 +38,7 @@ export default class ContentGrid extends React.Component {
   }
 
   render() {
-    let { rowHeight, columns } = this.state;
-
-    let samples = [
-      {
-        id: "1",
-        title: "Building a Custom Node in Unity's Shader Graph",
-        img: "https://i.ytimg.com/vi/FeL4uWGdtUY/hqdefault.jpg"
-      },
-      {
-        id: "2",
-        title: "Using Properties and Steps in Part 2 of our Ripple in Shader Graph",
-        img: "https://i.ytimg.com/vi/tL3GbBAqF3E/hqdefault.jpg"
-      },
-      {
-        id: "3",
-        title: "Creating and Using SSH RSA Tokens on Windows 10",
-        img: "https://i.ytimg.com/vi/gM2J2DWCTnM/hqdefault.jpg"
-      },
-      {
-        id: "4",
-        title: "Learning to Use Step and Smoothstep in Shaders",
-        img: "https://i.ytimg.com/vi/-vGNd8pnbYs/hqdefault.jpg"
-      },
-      {
-        id: "5",
-        title: "Point and Ripple - Ripple Shader - Part 2",
-        img: "https://i.ytimg.com/vi/UVdXUXv3Bw8/hqdefault.jpg"
-      },
-      {
-        id: "6",
-        title: "Live Coding a 3D Voxel Cube in Unity",
-        img: "https://i.ytimg.com/vi/laBPpyXfBWo/hqdefault.jpg"
-      }
-    ]
-
-    var videos = [];
-    for(var i = 0; i < 100; ++i) {
-      for(var n = 0; n < samples.length; ++n) {
-        videos.push(samples[n]);
-      }
-    }
+    const { videos, rowHeight, columns } = this.state;
 
     return (
       <Measure
@@ -76,7 +51,7 @@ export default class ContentGrid extends React.Component {
           <div style={{ margin: '4px' }} ref={measureRef}>
             <GridList cellHeight={rowHeight} cols={columns}>
               {
-                videos.map((item) => <GridListTile key={item.id}><ContentGridItem title={item.title} img={item.img}/></GridListTile>)
+                videos.map((item) => <GridListTile key={item.id}><a href={'/?v='+item.id}><ContentGridItem title={item.title} img={item.img}/></a></GridListTile>)
               }
             </GridList>
           </div>
