97218652b059ec876f12a247e7bcb26acae69951
Sam Wronski <sam@worldofzero.com>
2019-03-31 12:46:30 -0700

    Add GitHub to social, wrap on ultra-small screens.

--- frontend/components/SocialToolbar.js
+++ frontend/components/SocialToolbar.js
@@ -3,6 +3,7 @@ import { Button, Grid, Toolbar } from '@material-ui/core';
 import DiscordIcon from '../components/DiscordIcon';
 import TwitterIcon from '../components/TwitterIcon';
 import YouTubeIcon from '../components/YouTubeIcon';
+import GitHubIcon from '../components/GitHubIcon';
 
 const styles = theme => ({
   button: {
@@ -19,13 +20,16 @@ class SocialToolbar extends React.Component {
     return (
       <Toolbar>
         <Grid container>
-          <Grid item xs={4}>
+          <Grid item sm={3} xs={6}>
             <Button className={classes.button} style={{ backgroundColor: '#FF0000' }}><YouTubeIcon height={48} width={48}/></Button>
           </Grid>
-          <Grid item xs={4}>
+          <Grid item sm={3} xs={6}>
+            <Button className={classes.button} style={{ backgroundColor: '#222222' }}><GitHubIcon height={48} width={48}/></Button>
+          </Grid>
+          <Grid item sm={3} xs={6}>
             <Button className={classes.button} style={{ backgroundColor: '#38A1F3' }}><TwitterIcon height={48} width={48}/></Button>
           </Grid>
-          <Grid item xs={4}>
+          <Grid item sm={3} xs={6}>
             <Button className={classes.button} style={{ backgroundColor: '#7289DA' }}><DiscordIcon height={48} width={48}/></Button>
           </Grid>
         </Grid>
