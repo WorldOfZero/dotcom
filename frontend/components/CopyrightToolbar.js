26e8decb3ec9a0b6c1ebb72a58b85537a8c05c21
Sam Wronski <sam@worldofzero.com>
2019-03-31 23:27:42 -0700

    Update copyright line to include GitHub link for the project.

--- frontend/components/CopyrightToolbar.js
+++ frontend/components/CopyrightToolbar.js
@@ -1,4 +1,5 @@
 import { Grid, Toolbar } from '@material-ui/core';
+import GitHubIcon from './GitHubIcon';
 
 export default class CopyrightToolbar extends React.Component {
 
@@ -6,11 +7,15 @@ export default class CopyrightToolbar extends React.Component {
     return (
       <Toolbar>
         <Grid container>
-          <Grid style={{ textAlign: 'left' }} item sm={6} xs={12}>
+          <Grid style={{ textAlign: 'left' }} item sm={5} xs={12}>
             © Copyright - Sam Wronski
           </Grid>
           <Grid style={{ textAlign: 'right' }} item sm={6} xs={12}>
-            Lets Make Something Awesome, Together!
+            <div><strong>Lets Make Something Awesome, Together!</strong></div>
+            <div>The source for this site is available on <a href='https://github.com/WorldOfZero/dotcom' style={{color: '#FFF'}}>GitHub</a> </div>
+          </Grid>
+          <Grid style={{ textAlign: 'left' }} item sm={1} xs={12}>
+            <a href='https://github.com/WorldOfZero/dotcom'><GitHubIcon/></a>
           </Grid>
         </Grid>
       </Toolbar>
