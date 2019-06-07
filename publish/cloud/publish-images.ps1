0fcb279116841159408df1e40831b138b53af1d5
Sam Wronski <sam@worldofzero.com>
2019-06-06 23:47:07 -0700

    rename publish and compose service name

--- publish/cloud/publish-images.ps1
+++ publish/cloud/publish-images.ps1
@@ -1,9 +1,9 @@
 # 192.168.1.58:5000 is the link to a local Docker Repository
 # That repository was configured with: https://github.com/docker/docker.github.io/blob/master/registry/deploying.md
-docker build -t worldofzero.azurecr.io/woz-server:latest ../../backend/youtube-service
+docker build -t worldofzero.azurecr.io/woz-youtube-service:latest ../../backend/youtube-service
 docker build -t worldofzero.azurecr.io/woz-frontend:latest ../../frontend
 docker build -t worldofzero.azurecr.io/woz-proxy:latest ../../backend/proxy
 
-docker push worldofzero.azurecr.io/woz-server:latest
+docker push worldofzero.azurecr.io/woz-youtube-service:latest
 docker push worldofzero.azurecr.io/woz-frontend:latest
 docker push worldofzero.azurecr.io/woz-proxy:latest
