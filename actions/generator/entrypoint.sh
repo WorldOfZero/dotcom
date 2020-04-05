d2fd4578915fad039a1d3cb121ab7e507a04c0ce
Sam Wronski <sam@worldofzero.com>
2020-04-05 00:00:24 -0700

    Use app directory

--- actions/generator/entrypoint.sh
+++ actions/generator/entrypoint.sh
@@ -3,4 +3,4 @@
 # time=$(date)
 # echo "::set-output name=time::$time"
 
-./generate-files-dotnet --youtube-apikey=$1 --template=$2 --channel=$3 --output=$4
+./app/generate-files-dotnet --youtube-apikey=$1 --template=$2 --channel=$3 --output=$4
