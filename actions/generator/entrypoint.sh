c0e857d5b24ad1dd63e9f3b27cb0130ebb57982a
Sam Wronski <sam@worldofzero.com>
2020-04-04 23:58:29 -0700

    Remove dll and dotnet

--- actions/generator/entrypoint.sh
+++ actions/generator/entrypoint.sh
@@ -3,4 +3,4 @@
 # time=$(date)
 # echo "::set-output name=time::$time"
 
-dotnet generate-files-dotnet.dll -- --youtube-apikey=$1 --template=$2 --channel=$3 --output=$4
+./generate-files-dotnet --youtube-apikey=$1 --template=$2 --channel=$3 --output=$4
